28a1c7fc36f43031b7b8b911d52f915193dbd4c8
Tulani <tulanidepaula@gmail.com>
2024-03-08 21:55:38 -0300

    incluindo um contador

--- one_vite/src/App.jsx
+++ one_vite/src/App.jsx
@@ -1,7 +1,11 @@
+import React from "react"
 
 function App(){
   return(
-    <h2>Trem pequeno mas a dor de cabeça, não.</h2>
+    <>
+      <h2>Trem pequeno mas a dor de cabeça, não.</h2>
+      <h2 className>teste do class</h2>
+    </>
   )
 }
 export default App
